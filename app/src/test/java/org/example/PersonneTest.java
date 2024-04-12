6016d00614283e4f7afa0030b75af6484c62ece3
najem99A <alaa.najem999@gmail.com>
2024-04-12 12:35:09 +0200

    Création de la classe Personne  avec les attributs, le constructeurs, les accesseurs ainsi que mise en place des tests unitaires pour cette classe

---
+++ app/src/test/java/org/example/PersonneTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class PersonneTest {
+
+    @Test
+    public void testConstructorAndGetters() {
+        // Création d'une personne
+        Personne personne = new Personne(123456789, "Doe");
+
+        // Vérification des valeurs des attributs
+        assertEquals(123456789, personne.getPers_id());
+        assertEquals("Doe", personne.getNom());
+    }
+
+    @Test
+    public void testSetters() {
+        // Création d'une personne
+        Personne personne = new Personne(0, "");
+
+        // Modification des attributs
+        personne.setPers_id(987654321);
+        personne.setNom("Smith");
+
+        // Vérification des nouvelles valeurs
+        assertEquals(987654321, personne.getPers_id());
+        assertEquals("Smith", personne.getNom());
+    }
+}
