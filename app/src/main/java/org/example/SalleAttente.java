3b92c29198ebf471d03ff25517f92707bf8e6dac
najem99A <alaa.najem999@gmail.com>
2024-04-14 16:54:02 +0200

    Ajout de la classe RefToSalleAttenteNormale avec les methodes de set et get et la classe Test correspondante

--- app/src/main/java/org/example/SalleAttente.java
+++ app/src/main/java/org/example/SalleAttente.java
@@ -5,12 +5,12 @@ public class SalleAttente {
     private String nom_s;
     private String localisation_s;
     private int nb_Places_s;
- 
-    public SalleAttente(int no_s, String nom_s, String localisation_s, int nb_Places_s ) {
+
+    public SalleAttente(int no_s, String nom_s, String localisation_s, int nb_Places_s) {
         this.no_s = no_s;
         this.nom_s = nom_s;
         this.localisation_s = localisation_s;
-        this.nb_Places_s= nb_Places_s;
+        this.nb_Places_s = nb_Places_s;
     }
 
     public int getNo_s() {
@@ -37,11 +37,11 @@ public class SalleAttente {
         this.localisation_s = localisation_s;
     }
 
-    public int getnb_Places_s() {
+    public int getNb_Places_s() {
         return nb_Places_s;
     }
 
-    public void setnb_Places_s(int nb_Places_s) {
+    public void setNb_Places_s(int nb_Places_s) {
         this.nb_Places_s = nb_Places_s;
     }
 
